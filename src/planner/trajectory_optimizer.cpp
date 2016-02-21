60e10458dcb505e22696d582e6bf3ebb06d76914
Abbas <abbas.sadat@gmail.com>
2016-02-20 17:25:24 -0800

    ipopt

---
+++ src/planner/trajectory_optimizer.cpp
@@ -0,0 +1,18 @@
+#include "planner/trajectory_optimizer.h"
+
+namespace asn
+{
+
+trajectory_optimizer::trajectory_optimizer()
+{
+
+}
+
+trajectory_optimizer::~trajectory_optimizer()
+{
+
+}
+
+
+
+}
